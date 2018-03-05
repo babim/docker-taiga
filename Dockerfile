d2606958dc6f600b26747db499be2167f406e7cc
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-03-06 00:53:52 +0700

    1

---
+++ Dockerfile
@@ -0,0 +1,86 @@
+FROM python:3.4.5-alpine
+MAINTAINER Babim <babim@matmagoc.com>
+
+# Define build arguments: Taiga version
+ARG VERSION=3.1.0
+
+# Install necessary packages
+RUN apk update --no-cache &&\
+    apk add --no-cache ca-certificates wget git postgresql-dev musl-dev gcc jpeg-dev zlib-dev libxml2-dev libxslt-dev libffi-dev &&\
+    update-ca-certificates
+
+# Download taiga.io backend and frontend
+RUN mkdir -p /taiga.io/
+WORKDIR /taiga.io
+RUN wget https://github.com/taigaio/taiga-back/archive/$VERSION.tar.gz
+RUN tar xzf $VERSION.tar.gz
+RUN ln -sf taiga-back-$VERSION taiga-back
+RUN rm -f $VERSION.tar.gz
+
+# Install all required dependencies of the backend (we will check on container startup whether we need
+# to setup the database first)
+WORKDIR /taiga.io/taiga-back-$VERSION
+ENV LIBRARY_PATH=/lib:/usr/lib
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Setup default environmento
+ENV TAIGA_SSL "false"
+ENV TAIGA_HOSTNAME "localhost"
+ENV TAIGA_SECRET_KEY `< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-64};echo;`
+ENV TAIGA_DB_HOST "localhost"
+ENV TAIGA_DB_NAME "postgres"
+ENV TAIGA_DB_USER "postgres"
+ENV TAIGA_DB_PASSWORD "!!!PLEASE-REPLACE-ME!!!"
+ENV TAIGA_PUBLIC_REGISTER_ENABLED "false"
+ENV TAIGA_BACKEND_DEBUG "false"
+ENV TAIGA_FRONTEND_DEBUG "false"
+ENV TAIGA_FEEDBACK_ENABLED "false"
+ENV TAIGA_DEFAULT_LANGUAGE "en"
+ENV TAIGA_DEFAULT_THEME "material-design"
+
+# Active Directory configuration
+RUN apk add --no-cache krb5-dev openldap-dev
+RUN cd /taiga.io && git clone https://github.com/stemid/taiga-contrib-ad-auth && \
+    python /taiga.io/taiga-contrib-ad-auth/setup.py install
+ENV AD_ENABLE "false"
+ENV AD_REALM "DOMAIN.LAN"
+ENV AD_ALLOWED_DOMAINS "ad.domain.lan"
+ENV AD_LDAP_SERVER "ldaps://ad.domain.lan/"
+ENV AD_LDAP_PORT 636
+ENV AD_SEARCH_BASE "ou=Company,dc=ad,dc=lan"
+ENV AD_EMAIL_PROPERTY "mail"
+ENV AD_SEARCH_FILTER ""
+ENV AD_BIND_DN ""
+ENV AD_BIND_PASSWORD ""
+
+# LDAP configuration
+RUN pip install taiga-contrib-ldap-auth
+ENV LDAP_ENABLE "false"
+ENV LDAP_SERVER ""
+ENV LDAP_PORT 389
+ENV LDAP_BIND_DN ""
+ENV LDAP_BIND_PASSWORD ""
+ENV LDAP_SEARCH_BASE ""
+ENV LDAP_SEARCH_PROPERTY "sAMAccountName"
+ENV LDAP_EMAIL_PROPERTY = 'mail'
+ENV LDAP_FULL_NAME_PROPERTY = 'displayName'
+
+RUN python manage.py collectstatic --noinput
+RUN mkdir /taiga.io/presets
+COPY local.py /taiga.io/presets/local.py
+
+# Remove all packages that are not required anymore
+RUN apk del gcc wget git musl-dev libxml2-dev
+RUN apk add gettext
+
+# Copy files for startup
+COPY checkdb.py /taiga.io/checkdb.py
+COPY entrypoint.sh /taiga.io/entrypoint.sh
+
+# Create a data-directory into which the configuration files will be moved
+RUN mkdir /taiga.io/data
+
+# Startup
+WORKDIR /taiga.io/taiga-back
+ENTRYPOINT ["/taiga.io/entrypoint.sh"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ local.py
@@ -0,0 +1,69 @@
+# Please modify this file as needed, see the local.py.example for details:
+# https://github.com/taigaio/taiga-back/blob/master/settings/local.py.example
+
+from .common import *
+from .original import *
+
+# Set configured database parameters
+DATABASES['default']['NAME'] = os.getenv('TAIGA_DB_NAME')
+DATABASES['default']['HOST'] = os.getenv('POSTGRES_PORT_5432_TCP_ADDR') or os.getenv('TAIGA_DB_HOST')
+DATABASES['default']['USER'] = os.getenv('TAIGA_DB_USER')
+DATABASES['default']['PASSWORD'] = os.getenv('POSTGRES_ENV_POSTGRES_PASSWORD') or os.getenv('TAIGA_DB_PASSWORD')
+DATABASES['default']['PORT'] = 5432
+
+# Configure hostname and URLs
+SITES['api']['domain'] = os.getenv('TAIGA_HOSTNAME')
+SITES['front']['domain'] = os.getenv('TAIGA_HOSTNAME')
+MEDIA_URL  = 'http://' + os.getenv('TAIGA_HOSTNAME') + '/media/'
+STATIC_URL = 'http://' + os.getenv('TAIGA_HOSTNAME') + '/static/'
+
+# If running on SSL externally, change scheme and URLs accordingly
+if os.getenv('TAIGA_SSL').lower() == 'true':
+    SITES['api']['scheme'] = 'https'
+    SITES['front']['scheme'] = 'https'
+    MEDIA_URL  = 'https://' + os.getenv('TAIGA_HOSTNAME') + '/media/'
+    STATIC_URL = 'https://' + os.getenv('TAIGA_HOSTNAME') + '/static/'
+
+SECRET_KEY = os.getenv('TAIGA_SECRET_KEY')
+
+# Enable or disable public registration
+PUBLIC_REGISTER_ENABLED = (os.getenv('TAIGA_PUBLIC_REGISTER_ENABLED').lower() == 'true')
+
+# Enable or disable debugging
+DEBUG = (os.getenv('TAIGA_BACKEND_DEBUG').lower() == 'true')
+TEMPLATE_DEBUG = (os.getenv('TAIGA_BACKEND_DEBUG').lower() == 'true')
+
+# Configure LDAP backend (if enabled)
+if os.getenv('LDAP_ENABLE').lower() == 'true':
+    INSTALLED_APPS += ["taiga_contrib_ldap_auth"]
+    LDAP_SERVER = os.getenv('LDAP_SERVER')
+    LDAP_PORT = int(os.getenv('LDAP_PORT'))
+    # Full DN of the service account use to connect to LDAP server and search for login user's account entry
+    # If LDAP_BIND_DN is not specified, or is blank, then an anonymous bind is attempated
+    LDAP_BIND_DN = os.getenv('LDAP_BIND_DN')
+    LDAP_BIND_PASSWORD = os.getenv('LDAP_BIND_PASSWORD')
+    # Starting point within LDAP structure to search for login user
+    LDAP_SEARCH_BASE = os.getenv('LDAP_SEARCH_BASE')
+    # LDAP property used for searching, ie. login username needs to match value in sAMAccountName property in LDAP
+    LDAP_SEARCH_PROPERTY = os.getenv('LDAP_SEARCH_PROPERTY')
+    LDAP_SEARCH_SUFFIX = None
+    # Names of LDAP properties on user account to get email and full name
+    LDAP_EMAIL_PROPERTY = os.getenv('LDAP_EMAIL_PROPERTY')
+    LDAP_FULL_NAME_PROPERTY = os.getenv('LDAP_FULL_NAME_PROPERTY')
+
+# Configure AD LDAP backend (if enabled)
+if os.getenv('AD_ENABLE').lower() == 'true':
+    INSTALLED_APPS += ["taiga_contrib_ad_auth"]
+    AD_LDAP_SERVER = os.getenv('AD_LDAP_SERVER')
+    AD_LDAP_PORT = int(os.getenv('AD_LDAP_PORT'))
+    # Full DN of the service account use to connect to LDAP server and search for login user's account entry
+    # If LDAP_BIND_DN is not specified, or is blank, then an anonymous bind is attempated
+    AD_REALM = os.getenv('AD_REALM')
+    AD_ALLOWED_DOMAINS = os.getenv('AD_ALLOWED_DOMAINS')
+    AD_BIND_DN = os.getenv('AD_BIND_DN')
+    AD_BIND_PASSWORD = os.getenv('AD_BIND_PASSWORD')
+    # Starting point within LDAP structure to search for login user
+    AD_SEARCH_FILTER = os.getenv('AD_SEARCH_FILTER')
+    AD_SEARCH_BASE = os.getenv('AD_SEARCH_BASE')
+    # Names of LDAP properties on user account to get email and full name
+    AD_EMAIL_PROPERTY = os.getenv('AD_EMAIL_PROPERTY')
