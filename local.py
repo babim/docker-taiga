8b9f7855777f9233780d4e07fc40c24ee3ad1180
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-03-22 11:47:11 +0700

    1

--- local.py
+++ local.py
@@ -33,6 +33,13 @@ PUBLIC_REGISTER_ENABLED = (os.getenv('TAIGA_PUBLIC_REGISTER_ENABLED').lower() ==
 DEBUG = (os.getenv('TAIGA_BACKEND_DEBUG').lower() == 'true')
 TEMPLATE_DEBUG = (os.getenv('TAIGA_BACKEND_DEBUG').lower() == 'true')
 
+# Configure Email SMTP (if enabled)
+if os.getenv('EMAIL_ENABLE').lower() == 'true':
+    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_PORT = os.getenv('EMAIL_PORT')
+
 # Configure LDAP backend (if enabled)
 if os.getenv('LDAP_ENABLE').lower() == 'true':
     INSTALLED_APPS += ["taiga_contrib_ldap_auth"]
@@ -51,26 +58,26 @@ if os.getenv('LDAP_ENABLE').lower() == 'true':
     LDAP_EMAIL_PROPERTY = os.getenv('LDAP_EMAIL_PROPERTY')
     LDAP_FULL_NAME_PROPERTY = os.getenv('LDAP_FULL_NAME_PROPERTY')
 
-# # Configure AD LDAP backend (if enabled)
-# if os.getenv('AD_ENABLE').lower() == 'true':
-#     INSTALLED_APPS += ["taiga_contrib_ad_auth"]
-#     AD_LDAP_SERVER = os.getenv('AD_LDAP_SERVER')
-#     AD_LDAP_PORT = int(os.getenv('AD_LDAP_PORT'))
-#     # Full DN of the service account use to connect to LDAP server and search for login user's account entry
-#     # If LDAP_BIND_DN is not specified, or is blank, then an anonymous bind is attempated
-#     AD_REALM = os.getenv('AD_REALM')
-#     AD_ALLOWED_DOMAINS = os.getenv('AD_ALLOWED_DOMAINS')
-#     AD_BIND_DN = os.getenv('AD_BIND_DN')
-#     AD_BIND_PASSWORD = os.getenv('AD_BIND_PASSWORD')
-#     # Starting point within LDAP structure to search for login user
-#     AD_SEARCH_FILTER = os.getenv('AD_SEARCH_FILTER')
-#     AD_SEARCH_BASE = os.getenv('AD_SEARCH_BASE')
-#     # Names of LDAP properties on user account to get email and full name
-#     AD_EMAIL_PROPERTY = os.getenv('AD_EMAIL_PROPERTY')
+# Configure AD LDAP backend (if enabled)
+if os.getenv('AD_ENABLE').lower() == 'true':
+    INSTALLED_APPS += ["taiga_contrib_ad_auth"]
+    AD_LDAP_SERVER = os.getenv('AD_LDAP_SERVER')
+    AD_LDAP_PORT = int(os.getenv('AD_LDAP_PORT'))
+    # Full DN of the service account use to connect to LDAP server and search for login user's account entry
+    # If LDAP_BIND_DN is not specified, or is blank, then an anonymous bind is attempated
+    AD_REALM = os.getenv('AD_REALM')
+    AD_ALLOWED_DOMAINS = os.getenv('AD_ALLOWED_DOMAINS')
+    AD_BIND_DN = os.getenv('AD_BIND_DN')
+    AD_BIND_PASSWORD = os.getenv('AD_BIND_PASSWORD')
+    # Starting point within LDAP structure to search for login user
+    AD_SEARCH_FILTER = os.getenv('AD_SEARCH_FILTER')
+    AD_SEARCH_BASE = os.getenv('AD_SEARCH_BASE')
+    # Names of LDAP properties on user account to get email and full name
+    AD_EMAIL_PROPERTY = os.getenv('AD_EMAIL_PROPERTY')
 
-# # Configure KRB5 LDAP backend (if enabled)
-# if os.getenv('KRB5_ENABLE').lower() == 'true':
-#     INSTALLED_APPS += ["taiga_contrib_kerberos_auth"]
-#     KRB5_REALM = os.getenv('KRB5_REALM')
-#     KRB5_DOMAINS = os.getenv('KRB5_DOMAINS')
-#     KRB5_DEFAULT_DOMAIN = os.getenv('KRB5_DEFAULT_DOMAIN')
+# Configure KRB5 LDAP backend (if enabled)
+if os.getenv('KRB5_ENABLE').lower() == 'true':
+    INSTALLED_APPS += ["taiga_contrib_kerberos_auth"]
+    KRB5_REALM = os.getenv('KRB5_REALM')
+    KRB5_DOMAINS = os.getenv('KRB5_DOMAINS')
+    KRB5_DEFAULT_DOMAIN = os.getenv('KRB5_DEFAULT_DOMAIN')
