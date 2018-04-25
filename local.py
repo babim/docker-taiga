cddd4dbc2a88524b3d7ea10a2fbdd5316b4a6b48
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-04-25 23:23:00 +0700

    1

--- local.py
+++ local.py
@@ -37,10 +37,14 @@ TEMPLATE_DEBUG = (os.getenv('TAIGA_BACKEND_DEBUG').lower() == 'true')
 # Configure Email SMTP (if enabled)
 if os.getenv('EMAIL_ENABLE').lower() == 'true':
     EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-    EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+#    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+#    EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+#    EMAIL_PORT = os.getenv('EMAIL_PORT')
+    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+    EMAIL_USE_TLS = True
+#    EMAIL_USE_SSL = True
+    EMAIL_PORT = 587
     EMAIL_HOST = os.getenv('EMAIL_HOST')
-    EMAIL_PORT = os.getenv('EMAIL_PORT')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
